9ce8b889b74003f6d2ed1eb77b1eac35c23d10c4
Emmanuel Ochoa <eorivera95@gmail.com>
2023-12-03 23:18:59 -0600

    fix: wrapping with providers the render

--- src/components/TransactionsList/TransactionsList.test.tsx
+++ src/components/TransactionsList/TransactionsList.test.tsx
@@ -1,28 +1,24 @@
 import { render, screen } from '@testing-library/react'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeAll } from 'vitest'
 import TransactionsList from './TransactionsList'
+import { TransactionsProvider } from '../../contexts/TransactionsContext'
+import { ModalProvider } from '../../contexts/ModalContext'
+
+const MockProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <TransactionsProvider>
+      <ModalProvider>{children}</ModalProvider>
+    </TransactionsProvider>
+  )
+}
 
 describe('TransactionsList', () => {
-  it('should render the component without problems', () => {
-    render(<TransactionsList />)
-  })
-
-  it('should display a message if there is no transactions', () => {
-    render(<TransactionsList />)
-
-    const paragraphElement = screen.getByText(
-      /No hay transacciones para mostrar/i
+  beforeAll(() => {
+    render(
+      <MockProviders>
+        <TransactionsList />
+      </MockProviders>
     )
-
-    expect(paragraphElement).toBeInTheDocument()
   })
-
-  it('should display a transaction list', () => {
-    render(<TransactionsList />)
-    const paragraphElement = screen.getByText(
-      /No hay transacciones para mostrar/i
-    )
-
-    expect(paragraphElement).not.toBeInTheDocument()
-  })
+  it('should render the component without problems', () => {})
 })
