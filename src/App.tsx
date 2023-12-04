1139fbdaf471b250f24a460ff9576d95d2fea05c
Emmanuel Ochoa <eorivera95@gmail.com>
2023-12-03 22:12:44 -0600

    feat: display of button and formmodal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { useModalContext } from './contexts/ModalContext'
 import Balance from './components/Balance/Balance'
+import Button from './components/Button/Button'
 import Header from './components/Header/Header'
 import TransactionsList from './components/TransactionsList/TransactionsList'
+import FormModal from './components/FormModal/FormModal'
 
 function App() {
+  const { toggleModal } = useModalContext()
+
   return (
     <>
       <Header />
@@ -10,6 +15,22 @@ function App() {
         <Balance />
         <TransactionsList />
       </div>
+      <Button
+        style={{
+          maxWidth: '400px',
+          width: '100%',
+          position: 'fixed',
+          left: '50%',
+          bottom: '2%',
+          transform: 'translateX(-50%)',
+          padding: '1rem 4rem',
+        }}
+        handleClick={toggleModal}
+      >
+        Agregar Movimiento
+      </Button>
+
+      <FormModal />
     </>
   )
 }
