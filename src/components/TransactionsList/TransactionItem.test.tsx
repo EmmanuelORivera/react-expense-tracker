55403c35c783f66fb35464eb44786930699eb793
Emmanuel Ochoa <eorivera95@gmail.com>
2023-12-03 23:24:08 -0600

    fix: wrapping with provider

--- src/components/TransactionsList/TransactionItem.test.tsx
+++ src/components/TransactionsList/TransactionItem.test.tsx
@@ -1,22 +1,35 @@
 import { render, screen } from '@testing-library/react'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import TransactionItem from './TransactionItem'
 import { ITransaction } from '../../interfaces/ITransaction'
+import { TransactionsProvider } from '../../contexts/TransactionsContext'
+import { ModalProvider } from '../../contexts/ModalContext'
+
+const MockProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <TransactionsProvider>
+      <ModalProvider>{children}</ModalProvider>
+    </TransactionsProvider>
+  )
+}
 
 describe('TransactionItem', () => {
+  beforeEach(() => {
+    render(
+      <MockProviders>
+        <TransactionItem transaction={transaction} />
+      </MockProviders>
+    )
+  })
   const transaction: ITransaction = {
     id: '1',
     text: 'Example Transaction',
     amount: 100,
     date: new Date(),
   }
-  it('should render the component without a problem', () => {
-    render(<TransactionItem transaction={transaction} />)
-  })
+  it('should render the component without a problem', () => {})
   it('renders the transaction item correctly', () => {
-    render(<TransactionItem transaction={transaction} />)
-
-    const element = screen.getByText(/Example Transactions : 100/i)
+    const element = screen.getByText(/Example Transaction/i)
 
     expect(element).toBeInTheDocument()
   })
