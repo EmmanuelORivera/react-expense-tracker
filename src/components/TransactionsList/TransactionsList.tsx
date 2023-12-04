4376a2dc627246e0cbbc688c86234e27dc0be120
Emmanuel Ochoa <eorivera95@gmail.com>
2023-12-03 22:04:54 -0600

    refactor: using the context

--- src/components/TransactionsList/TransactionsList.tsx
+++ src/components/TransactionsList/TransactionsList.tsx
@@ -1,26 +1,15 @@
-import { useEffect, useState } from 'react'
-import { ITransaction } from '../../interfaces/ITransaction'
-import transactionsData from '../../data/transactions'
-import transactionsDate from '../../utils/transactionsDate'
 import TransactionGroup from './TransactionGroup'
+import { useTransactionsContext } from '../../contexts/TransactionsContext'
 
 const TransactionsList = () => {
-  const [transactions, setTransactions] = useState<
-    Record<string, ITransaction[]>
-  >({})
-
-  useEffect(() => {
-    const groupedTransactions =
-      transactionsDate.groupTransactions(transactionsData)
-    setTransactions(groupedTransactions)
-  }, [])
+  const { transactions } = useTransactionsContext()
 
   return (
     <div data-testid="transactions-list">
       {Object.keys(transactions).length === 0 ? (
         <p>No hay transacciones para mostrar</p>
       ) : (
-        <div className="mt-5">
+        <div className="mt-5" style={{ marginBottom: '120px' }}>
           {Object.entries(transactions).map(([date, transactionList]) => (
             <TransactionGroup
               key={date}
